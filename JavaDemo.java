f7d20f1544de1642c7a64f2d24e68b3ce5bc2210
Towsif824 <tawsif824@gmail.com>
2022-04-21 16:44:36 +0600

    greater number

--- JavaDemo.java
+++ JavaDemo.java
@@ -2,5 +2,35 @@ public class JavaDemo {
     public static void main(String args[])  //static method
     {
         System.out.println("Hello Test");
+         game(7,2,9);
+         int x =1, y=2;
+         System.out.println(x>y);
     }
+
+    public static void game(int a, int b, int c)
+    {
+        if(a>b)
+        {
+            if(a>c)
+            {
+                System.out.println("The bigger number is" + a);
+            }
+            else
+            {
+                System.out.println("the biggest number is " + c);
+            }
+        }
+        else
+        {
+            if(b>c)
+            {
+                System.out.println("The biggest number is"+ b);
+            }
+            else
+            {
+                System.out.println("The biggest number is"+ c);
+            }
+        }
+    }
+
 }
